9820642f95a061a17a5208093e011f6d977ce813
Ekaterina <ket2019@mail.ru>
2021-11-28 22:28:23 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// alert('Привет');
+// const result  = confirm("Вам исполнилось 18 лет?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "нет");
+// console.log(answer + 10);
+const answers = [];
+answers[0] = prompt('Ваше имя?', '');
+answers[1] = prompt('Ваша фамилия?', '');
+answers[2] = prompt('Ваш возраст?', '');
+
+console.log(answers);
