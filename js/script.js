d615df8e7888e62e88aad4a31c436b26838946dc
Ekaterina <ket2019@mail.ru>
2021-11-29 19:10:22 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 "use strict";
 
-// alert('Привет');
-// const result  = confirm("Вам исполнилось 18 лет?");
-// console.log(result);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const answer = prompt("Вам есть 18?", "нет");
-// console.log(answer + 10);
-const answers = [];
-answers[0] = prompt('Ваше имя?', '');
-answers[1] = prompt('Ваша фамилия?', '');
-answers[2] = prompt('Ваш возраст?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log(answers);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
